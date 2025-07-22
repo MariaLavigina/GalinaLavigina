57ba5595acccdf07e8cce00ea5aeb7304a31d638
your_username <135663272+MariaLavigina@users.noreply.github.com>
2025-07-22 18:38:03 +0100

    two buttons added

--- load-footer.js
+++ load-footer.js
@@ -1,9 +1,13 @@
-fetch("footer-mobile.html")
+fetch("footer.html")
   .then((response) => response.text())
   .then((data) => {
-    console.log("Fetched footer length:", data.length);
-    console.log(data); // Full output!
-    console.log(document.getElementById("footer-placeholder").innerHTML);
     document.getElementById("footer-placeholder").innerHTML = data;
   })
-  .catch((error) => console.error("Error loading footer:", error));
+  .catch((error) => console.error("Error loading mobile footer:", error));
+
+// fetch("footer-desktop.html")
+//   .then((response) => response.text())
+//   .then((data) => {
+//     document.getElementById("footer-desktop-placeholder").innerHTML = data;
+//   })
+//   .catch((error) => console.error("Error loading desktop footer:", error));
